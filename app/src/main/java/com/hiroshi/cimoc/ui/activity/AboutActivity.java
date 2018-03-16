27e1989b16a5e9fd37c2da9641d5adbc17f8107b
feilong <feilongphone@gmail.com>
2018-03-16 14:41:14 +0800

    change about

--- app/src/main/java/com/hiroshi/cimoc/ui/activity/AboutActivity.java
+++ app/src/main/java/com/hiroshi/cimoc/ui/activity/AboutActivity.java
@@ -43,65 +43,65 @@ public class AboutActivity extends BackActivity implements AboutView {
     protected void initView() {
         try {
             PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), 0);
-            mVersionName.setText(StringUtils.format("version: %s", info.versionName));
+            mVersionName.setText(StringUtils.format("version: unKnown"));
         } catch (Exception e){
             e.printStackTrace();
         }
     }
 
     @OnClick(R.id.about_support_btn) void onSupportClick() {
-        ClipboardManager manager = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-        manager.setPrimaryClip(ClipData.newPlainText(null, getString(R.string.about_support_email)));
-        showSnackbar(R.string.about_already_clip);
+//        ClipboardManager manager = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+//        manager.setPrimaryClip(ClipData.newPlainText(null, getString(R.string.about_support_email)));
+//        showSnackbar(R.string.about_already_clip);
     }
 
     @OnClick(R.id.about_resource_btn) void onResourceClick() {
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.about_resource_url)));
-        try {
-            startActivity(intent);
-        } catch (Exception e) {
-            showSnackbar(R.string.about_resource_fail);
-        }
+//        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.about_resource_url)));
+//        try {
+//            startActivity(intent);
+//        } catch (Exception e) {
+//            showSnackbar(R.string.about_resource_fail);
+//        }
     }
 
     @OnClick(R.id.about_update_btn) void onUpdateClick() {
-        if (update) {
-            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.about_update_url)));
-            try {
-                startActivity(intent);
-            } catch (Exception e) {
-                showSnackbar(R.string.about_resource_fail);
-            }
-        } else if (!checking) {
-            try {
-                PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), 0);
-                mUpdateText.setText(R.string.about_update_doing);
-                checking = true;
-                mPresenter.checkUpdate(info.versionName);
-            } catch (Exception e){
-                mUpdateText.setText(R.string.about_update_fail);
-                checking = false;
-            }
-        }
+//        if (update) {
+//            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.about_update_url)));
+//            try {
+//                startActivity(intent);
+//            } catch (Exception e) {
+//                showSnackbar(R.string.about_resource_fail);
+//            }
+//        } else if (!checking) {
+//            try {
+//                PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), 0);
+//                mUpdateText.setText(R.string.about_update_doing);
+//                checking = true;
+//                mPresenter.checkUpdate(info.versionName);
+//            } catch (Exception e){
+//                mUpdateText.setText(R.string.about_update_fail);
+//                checking = false;
+//            }
+//        }
     }
 
     @Override
     public void onUpdateNone() {
-        mUpdateText.setText(R.string.about_update_latest);
-        checking = false;
+//        mUpdateText.setText(R.string.about_update_latest);
+//        checking = false;
     }
 
     @Override
     public void onUpdateReady() {
-        mUpdateText.setText(R.string.about_update_download);
-        checking = false;
-        update = true;
+//        mUpdateText.setText(R.string.about_update_download);
+//        checking = false;
+//        update = true;
     }
 
     @Override
     public void onCheckError() {
-        mUpdateText.setText(R.string.about_update_fail);
-        checking = false;
+//        mUpdateText.setText(R.string.about_update_fail);
+//        checking = false;
     }
 
     @Override
