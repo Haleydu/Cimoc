1682bde6bc51c6a390838093a4788e06f59b9c20
feilong <614183595@qq.com>
2018-04-03 09:52:42 +0800

    整理 Mongo.java

--- app/src/main/java/com/hiroshi/cimoc/core/Mongo.java
+++ app/src/main/java/com/hiroshi/cimoc/core/Mongo.java
@@ -3,6 +3,7 @@ package com.hiroshi.cimoc.core;
 import com.hiroshi.cimoc.model.Chapter;
 import com.hiroshi.cimoc.model.Comic;
 import com.hiroshi.cimoc.model.ImageUrl;
+import com.hiroshi.cimoc.utils.StringUtils;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoCollection;
@@ -10,7 +11,10 @@ import com.mongodb.client.MongoDatabase;
 
 import org.bson.Document;
 
+import java.sql.Time;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.ListIterator;
 
@@ -25,8 +29,8 @@ public class Mongo {
     private MongoCollection<Document> comicBaseColl;
     private MongoCollection<Document> comicChaColl;
 
-    private Document queryStr;
-    private Document setStr;
+//    private Document queryStr;
+//    private Document setStr;
 
     public Mongo(){
 //        mongoUrl = new MongoClientURI("mongodb://comic:ba93Z5qUerhSJE3q@ds014118.mlab.com:14118/comic");
@@ -50,29 +54,60 @@ public class Mongo {
         return chapterList;
     }
 
+    private Document QueryBaseDoc(int source,String mid){
+        Document queryStr = new Document("lid",source)
+            .append("mid",mid);
+        return comicBaseColl.find(queryStr).first();
+    }
+
+    private String getDate(){
+        Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+//        int hour = c.get(Calendar.HOUR_OF_DAY);
+//        int minute = c.get(Calendar.MINUTE);
+//        int second = c.get(Calendar.SECOND);
+//        int week_month = c.get(Calendar.WEEK_OF_MONTH);
+//        int week_year = c.get(Calendar.WEEK_OF_YEAR);
+//        int week_day = c.get(Calendar.DAY_OF_WEEK);
+//        int week_day_month = c.get(Calendar.DAY_OF_WEEK_IN_MONTH);
+        return StringUtils.format("%d%d%d",year,month,day);
+    }
+
+    private void InsertBaseByDoc(Comic comic, List<Chapter> list){
+        Document setStr = new Document("lid",comic.getSource())
+            .append("mid",comic.getCid())
+            .append("lastcid",list.get(0).getPath())
+//            .append("lastdate",getDate())
+            .append("lastdate",new Date())
+            .append("path",comic.getUrl())
+            .append("title",comic.getTitle())
+            .append("intro",comic.getIntro())
+            .append("author",comic.getAuthor())
+            .append("chapter", genChapterListFromDocumentList(list));
+        comicBaseColl.insertOne(setStr);
+    }
+
+    private void UpdateOneBase(int source,String mid,List<Chapter> list){
+        Document queryStr = new Document("lid",source)
+            .append("mid",mid);
+        Document setStr = new Document("lastcid",list.get(0).getPath())
+            .append("chapter", genChapterListFromDocumentList(list));
+        comicBaseColl.updateOne(queryStr, new Document("$set",setStr));
+    }
+
     public void UpdateComicBase(Comic comic, List<Chapter> list){
         try{
             //search
-            queryStr = new Document("lid",comic.getSource())
-                            .append("mid",comic.getCid());
-            Document d = comicBaseColl.find(queryStr).first();
+            Document d = QueryBaseDoc(comic.getSource(),comic.getCid());
             //if not exist,create it
             if(d == null){
-                setStr = new Document("lid",comic.getSource())
-                    .append("mid",comic.getCid())
-                    .append("lastcid",list.get(0).getPath())
-                    .append("path",comic.getUrl())
-                    .append("title",comic.getTitle())
-                    .append("intro",comic.getIntro())
-                    .append("author",comic.getAuthor())
-                    .append("chapter", genChapterListFromDocumentList(list));
-                comicBaseColl.insertOne(setStr);
+                InsertBaseByDoc(comic,list);
             }else
                 //if update,refersh it
                 if(!d.get("lastcid").equals(list.get(0).getPath())) {
-                    setStr = new Document("lastcid",list.get(0).getPath())
-                                .append("chapter", genChapterListFromDocumentList(list));
-                    comicBaseColl.updateOne(queryStr, new Document("$set",setStr));
+                    UpdateOneBase(comic.getSource(),comic.getCid(),list);
                 }
         }catch (Exception ex){
             //connect to databases error
@@ -108,13 +143,10 @@ public class Mongo {
                                    List<ImageUrl> list){
         try{
             //search
-            queryStr = new Document("lid",source)
-                .append("mid",mid)
-                .append("cid",cid);
-            Document d = comicChaColl.find(queryStr).first();
+            Document d = QueryCollDoc(source,mid,cid);
             //if not exist,create it
             if(d == null){
-                setStr = new Document("lid",source)
+                Document setStr = new Document("lid",source)
                     .append("mid",mid)
                     .append("cid",cid)
                     .append("pic", genDocumentListFromImageUrlList(list));
@@ -134,10 +166,7 @@ public class Mongo {
                              final String mid,
                              final String cid) {
         try {
-            queryStr = new Document("lid",source)
-                .append("mid",mid)
-                .append("cid",cid);
-            Document d = comicChaColl.find(queryStr).first();
+            Document d = QueryCollDoc(source,mid,cid);
             if(d==null){
                 return new ArrayList<>();
             }else{
@@ -148,5 +177,12 @@ public class Mongo {
             //connect to databases error
             return new ArrayList<>();
         }
+    }
+
+    private Document QueryCollDoc(int source,String mid,String cid){
+        Document queryStr = new Document("lid",source)
+            .append("mid",mid)
+            .append("cid",cid);
+        return comicChaColl.find(queryStr).first();
     }
 }
