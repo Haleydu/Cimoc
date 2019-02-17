1c6fa670c91374d174d072b854d23796e1e47ac6
Reborn <ren_xiaoyao@outlook.com>
2019-02-18 00:57:39 +0800

    修改OkHttp请求Https的Client实例部分代码（使Okhttp信任所有证书）

--- app/src/main/java/com/hiroshi/cimoc/App.java
+++ app/src/main/java/com/hiroshi/cimoc/App.java
@@ -29,8 +29,16 @@ import com.hiroshi.cimoc.utils.StringUtils;
 
 import org.greenrobot.greendao.identityscope.IdentityScopeType;
 
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
 import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import okhttp3.OkHttpClient;
 
@@ -83,7 +91,7 @@ public class App extends Application implements AppGetter, Thread.UncaughtExcept
             @Override
             public void onActivityStarted(Activity activity) {
 //                Log.d("ActivityLifecycle:",activity+"onActivityStarted");
-                sActivity=activity;
+                sActivity = activity;
 
             }
 
@@ -212,18 +220,53 @@ public class App extends Application implements AppGetter, Thread.UncaughtExcept
 
         if (mHttpClient == null) {
 
-            // 修改为粗暴访问https
-            mHttpClient = new OkHttpClient().newBuilder().hostnameVerifier(new HostnameVerifier() {
-
-                @Override
-                public boolean verify(String hostname, SSLSession session) {
-                    //强行返回true 即验证成功
-                    return true;
-                }
-            }).build();
+            // 3.OkHttp访问https的Client实例
+            mHttpClient = new OkHttpClient().newBuilder()
+                    .sslSocketFactory(createSSLSocketFactory())
+                    .hostnameVerifier(new TrustAllHostnameVerifier())
+                    .build();
         }
 
         return mHttpClient;
     }
+
+    // 1.实现X509TrustManager接口
+    private static class TrustAllCerts implements X509TrustManager {
+        @Override
+        public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+        }
+
+        @Override
+        public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+        }
+
+        @Override
+        public X509Certificate[] getAcceptedIssuers() {
+            return new X509Certificate[0];
+        }
+    }
+
+    // 2.实现HostnameVerifier接口
+    private static class TrustAllHostnameVerifier implements HostnameVerifier {
+        @Override
+        public boolean verify(String hostname, SSLSession session) {
+            return true;
+        }
+    }
+
+    private static SSLSocketFactory createSSLSocketFactory() {
+        SSLSocketFactory ssfFactory = null;
+
+        try {
+            SSLContext sc = SSLContext.getInstance("TLS");
+            sc.init(null, new TrustManager[]{new TrustAllCerts()}, new SecureRandom());
+
+            ssfFactory = sc.getSocketFactory();
+        } catch (Exception e) {
+        }
+
+        return ssfFactory;
+    }
+
 
 }
