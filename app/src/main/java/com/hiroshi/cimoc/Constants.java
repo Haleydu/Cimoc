ddb4450a700299f28d9139647c60546e2c966549
Reborn <ren_xiaoyao@outlook.com>
2019-03-19 15:29:25 +0800

    Update version to v1.5.6

--- app/src/main/java/com/hiroshi/cimoc/Constants.java
+++ app/src/main/java/com/hiroshi/cimoc/Constants.java
@@ -2,7 +2,7 @@ package com.hiroshi.cimoc;
 
 public final class Constants {
 
-    public static final int VERSION = 105001;
+    public static final int VERSION = 105006;
 
     public static final String UPDATE_GITHUB_URL = "https://api.github.com/repos/RebornQ/Cimoc/releases/latest";
 
