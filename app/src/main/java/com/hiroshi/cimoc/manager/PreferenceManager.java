9f0ad12876bb879d525d628e3ef19918ea6f4ea5
feilong <feilongphone@gmail.com>
2019-04-15 12:42:17 +0800

    add setting check box

--- app/src/main/java/com/hiroshi/cimoc/manager/PreferenceManager.java
+++ app/src/main/java/com/hiroshi/cimoc/manager/PreferenceManager.java
@@ -94,6 +94,7 @@ public class PreferenceManager {
     public static final String PREF_OTHER_THEME = "pref_other_theme";
     public static final String PREF_OTHER_LAUNCH = "pref_other_launch";
     public static final String PREF_OTHER_NIGHT_ALPHA = "pref_other_night_alpha";
+    public static final String PREF_OTHER_SHOW_TOPBAR = "pref_other_show_topbar";
 
     public static final String PREF_DOWNLOAD_THREAD = "pref_download_thread";
 
