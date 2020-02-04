e4a97ef6dd203604ebfd64bf96431f9e696dbcbb
feilong <feilongphone@gmail.com>
2020-02-04 20:38:11 +0800

    fix update checker o.o

--- app/src/main/java/com/hiroshi/cimoc/Constants.java
+++ app/src/main/java/com/hiroshi/cimoc/Constants.java
@@ -4,7 +4,7 @@ public final class Constants {
 
     public static final int VERSION = 105006;
 
-    public static final String UPDATE_GITHUB_URL = "https://api.github.com/repos/RebornQ/Cimoc/releases/latest";
+    public static final String UPDATE_GITHUB_URL = "https://api.github.com/repos/feilongfl/Cimoc/releases/latest";
 
-    public static final String UPDATE_GITEE_URL = "https://gitee.com/api/v5/repos/Reborn_0/Cimoc_Mirror/releases/latest";
+    public static final String UPDATE_GITEE_URL = "https://gitee.com/api/v5/repos/feilongfl/Cimoc_Mirror/releases/latest";
 }
