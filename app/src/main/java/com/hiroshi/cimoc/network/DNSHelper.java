71d75dafbfdbf923289ce61a9c1a1f52fc25cc6b
feilong <feilongphone@gmail.com>
2020-02-04 09:48:05 +0800

    disable manhuagui

--- app/src/main/java/com/hiroshi/cimoc/network/DNSHelper.java
+++ app/src/main/java/com/hiroshi/cimoc/network/DNSHelper.java
@@ -4,11 +4,11 @@ public class DNSHelper {
 
     static public String getIpByHost(String hostname) {
         switch (hostname) {
-            case "m.manhuagui.com":
-            case "tw.manhuagui.com":
-                return "47.89.23.88";
-            case "www.manhuagui.com":
-                return "14.49.38.185";
+            // case "m.manhuagui.com":
+            // case "tw.manhuagui.com":
+            //     return "47.89.23.88";
+            // case "www.manhuagui.com":
+            //     return "14.49.38.185";
             //return "106.185.40.107";
             default:
                 return null;
