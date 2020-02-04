8ffc98257aa2929aedbcc652824172e12e3e6980
feilong <feilongphone@gmail.com>
2020-02-04 19:49:53 +0800

    disable netease

--- app/src/main/java/com/hiroshi/cimoc/source/NetEase.java
+++ app/src/main/java/com/hiroshi/cimoc/source/NetEase.java
@@ -38,7 +38,7 @@ public class NetEase extends MangaParser {
     }
 
     public static Source getDefaultSource() {
-        return new Source(null, DEFAULT_TITLE, TYPE, true);
+        return new Source(null, DEFAULT_TITLE, TYPE, false);
     }
 
     @Override
