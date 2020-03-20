4e0f73d33bad19b2a008afb8e98f8a655b45d013
feilong <feilongphone@gmail.com>
2020-03-20 20:54:38 +0800

    Fix dmzj source in manhuadui.

--- app/src/main/java/com/hiroshi/cimoc/source/MH50.java
+++ app/src/main/java/com/hiroshi/cimoc/source/MH50.java
@@ -175,6 +175,8 @@ public class MH50 extends MangaParser {
         for (int i = 0; i != imageListSize; ++i) {
             String key = imageList.getString(i);
             String imageUrl = getImageUrlByKey(key, server[1], chapterPath);
+            if(imageUrl.contains("images.dmzj.com"))
+                imageUrl = "https://img01.eshanyao.com/showImage.php?url=" + imageUrl;
             list.add(new ImageUrl(i + 1, imageUrl, false));
         }
 
